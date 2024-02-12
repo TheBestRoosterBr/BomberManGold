caec437b54e0b2bd5e211f9dc96623bb79be2ab8
WalterHenri <walterdoideira5438@gmail.com>
2024-02-11 22:46:02 -0300

    copiacao das sprites para comeco

--- App.py
+++ App.py
@@ -5,6 +5,7 @@ class App:
     def __init__(self):
         pygame.init()
         self.isRunning = True
+        self.screen = pygame.display.set_mode((800,600))
 
     def run(self):
         while self.isRunning:
